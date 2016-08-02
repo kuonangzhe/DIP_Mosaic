28bc0b1c0c6102bb154b198a2d77b99eb5135120
kuonangzhe <15834119171@163.com>
2016-08-02 10:26:06 +0800

    Update MosaicMaker.m

--- MosaicMaker.m
+++ MosaicMaker.m
@@ -1,16 +1,23 @@
-x = imread('/Users/MaoChongzhao/Downloads/mi2.jpg');
+% This file is to make the Mosaic image
+% Author: Chongzhao Mao
+% Last Modifier: Chongzhao Mao
+
+mainImagePath = '/Users/MaoChongzhao/Downloads/mi2.jpg'; % add you own image path
+x = imread(mainImagePath);
+[m,n] = size(x);
 height = 30;
 width = 30;
-numh = 68;
-numw = 102;
+numh = floor(m/height); 
+numw = floor(n/width);
+weight = 0.5; % adjust the weight for your own interest
 for i = 1:numh
     i
     for j = 1:numw
         num = numh*(i-1)+j;
-        img = imagePool{num};
+        img = imagePool{num}; % imagePool is from preprocess file, after running preprocess file, you should have this
         yy = (height*(i-1)+1):(height*i);
         xx = (width*(j-1)+1):(width*j);
-        x(yy,xx,1:3) = 0.5*x(yy,xx,1:3) + img*0.5; 
+        x(yy,xx,1:3) = weight*x(yy,xx,1:3) + img*(1-weight); 
     end
 end
 figure, imshow(x)
